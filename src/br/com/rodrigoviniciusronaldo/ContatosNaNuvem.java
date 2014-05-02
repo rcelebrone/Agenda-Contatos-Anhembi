a17f1e52372ace44c4f62824b0a0609fd12a0de1
Rodrigo Celebrone <Rodrigo Celebrone@rcelebrone>
2014-05-02 16:54:46 -0300

    inicio do projeto do segundo semestre. /* * Rodrigo Celebrone - 20428584 * Ronaldo dos Santos Pereira - 20432398 * Vinicius Andrade - 20470284 */

---
+++ src/br/com/rodrigoviniciusronaldo/ContatosNaNuvem.java
@@ -0,0 +1,22 @@
+/*
+* Rodrigo Celebrone - 20428584
+* Ronaldo dos Santos Pereira - 20432398
+* Vinicius Andrade - 20470284
+*/
+
+package br.com.rodrigoviniciusronaldo;
+
+/**
+ *
+ * @author Rodrigo Celebrone
+ */
+public class ContatosNaNuvem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
