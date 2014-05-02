44aa20d4b5b6fb0c13f98bfa877aa6c060059e1b
Rodrigo Celebrone <Rodrigo Celebrone@rcelebrone>
2014-05-02 16:57:59 -0300

    inicio do projeto do segundo semestre.

--- src/br/com/rodrigoviniciusronaldo/ContatosNaNuvem.java
+++ src/br/com/rodrigoviniciusronaldo/ContatosNaNuvem.java
@@ -12,9 +12,6 @@ package br.com.rodrigoviniciusronaldo;
  */
 public class ContatosNaNuvem {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
