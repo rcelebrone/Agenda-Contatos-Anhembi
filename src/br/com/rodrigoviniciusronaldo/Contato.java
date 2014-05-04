954004677dcc405af0241e3c4e1dcb0ea9deed6e
Ronaldo <Ronaldo@192.168.1.31>
2014-05-04 16:00:43 -0300

    get and set

--- src/br/com/rodrigoviniciusronaldo/Contato.java
+++ src/br/com/rodrigoviniciusronaldo/Contato.java
@@ -12,16 +12,52 @@ public class Contato {
     private String endereco;
     private int telefonePessoal;
     private int telefoneComercial;
+
+// esse é o metodo set de nome, responsavel por receber um valor 
+// como parametro e colocar esse valor na propriedade nome (this.nome) da class
     
-    // esse é o metodo set de nome, responsavel por receber um valor 
-    // como parametro e colocar esse valor na propriedade nome (this.nome) da class
-    public void SetNome(String nome){
+    public void setNome(String nome) {
         this.nome = nome;
     }
-    
-    // esse metodo é o get de nome, responsavel por retornar
-    // o valor da propriedade nome (this.nome)
-    public String GetNome(){
-        return this.nome;
+
+    public void setEmail(String email) {
+        this.email = email;
     }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setTelefonePessoal(int telefonePessoal) {
+        this.telefonePessoal = telefonePessoal;
+    }
+
+    public void setTelefoneComercial(int telefoneComercial) {
+        this.telefoneComercial = telefoneComercial;
+    }
+
+    
+// esse metodo é o get de nome, responsavel por retornar
+    // o valor da propriedade nome (this.nome)
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public int getTelefonePessoal() {
+        return telefonePessoal;
+    }
+
+    public int getTelefoneComercial() {
+        return telefoneComercial;
+    }
+    
 }
