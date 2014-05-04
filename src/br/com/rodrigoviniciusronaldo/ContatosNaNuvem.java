f7b9e622aa10090afa75f825f9b4ff11a22b17ac
Rodrigo Celebrone <Rodrigo Celebrone@rcelebrone>
2014-05-04 16:28:07 -0300

    trocando system.out.println pro Label

--- src/br/com/rodrigoviniciusronaldo/ContatosNaNuvem.java
+++ src/br/com/rodrigoviniciusronaldo/ContatosNaNuvem.java
@@ -23,19 +23,19 @@ public class ContatosNaNuvem {
         listaContato = new ArrayList<>();
         
         while(menuPrograma != 6){
-            System.out.println("////////////////////Escolha uma opção////////////////////////");
-            System.out.println("1. Inserir novo contato");
-            System.out.println("2. Buscar um contato existente");
-            System.out.println("3. Exibir o nome de todos os contatos cadastrados");
-            System.out.println("4. Atualizar os dados de um contato já existente");
-            System.out.println("5. Remover contato");
-            System.out.println("6. Sair da aplicação");
-            System.out.println("////////////////////////////////////////////");
-            System.out.println("Digite o numero de uma dos opções acima: ");
+            Label("////////////////////Escolha uma opção////////////////////////");
+            Label("1. Inserir novo contato");
+            Label("2. Buscar um contato existente");
+            Label("3. Exibir o nome de todos os contatos cadastrados");
+            Label("4. Atualizar os dados de um contato já existente");
+            Label("5. Remover contato");
+            Label("6. Sair da aplicação");
+            Label("////////////////////////////////////////////");
+            Label("Digite o numero de uma dos opções acima: ");
             menuPrograma = input.nextInt();
 
             while (menuPrograma < 1 || menuPrograma > 6) {
-                System.out.print("Digite uma das opções 1,2,3,4,5,6: ");
+                Label("Digite uma das opções 1,2,3,4,5,6: ");
                 menuPrograma = input.nextInt();
             }
             
