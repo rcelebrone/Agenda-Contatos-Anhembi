b73464278dc8b5b86a685f4de4e619174df196f7
Rodrigo Celebrone <Rodrigo Celebrone@rcelebrone>
2014-05-04 15:23:36 -0300

    primeiro get e set

--- src/br/com/rodrigoviniciusronaldo/Contato.java
+++ src/br/com/rodrigoviniciusronaldo/Contato.java
@@ -12,4 +12,16 @@ public class Contato {
     private String endereco;
     private int telefonePessoal;
     private int telefoneComercial;
+    
+    // esse é o metodo set de nome, responsavel por receber um valor 
+    // como parametro e colocar esse valor na propriedade nome (this.nome) da class
+    public void SetNome(String nome){
+        this.nome = nome;
+    }
+    
+    // esse metodo é o get de nome, responsavel por retornar
+    // o valor da propriedade nome (this.nome)
+    public String GetNome(){
+        return this.nome;
+    }
 }
